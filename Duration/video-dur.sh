8a61cd84346b501888e9fdb62ad45c014dc31483
Mohamed Hafez <mohamedhafezmansour@gmail.com>
2022-07-07 03:17:18 +0200

    Calculate mp3 & mp4 duration

---
+++ Duration/video-dur.sh
@@ -0,0 +1,2 @@
+#! /usr/bin/bash
+find $1 -maxdepth 1 -iname "*.mp4" -exec ffprobe -v quiet -of csv=p=0 -show_entries format=duration {} \; | paste -sd+ -| bc | awk -F: '{printf "🎥 %02d:%02d:%02d\n",$1/3600,($1%3600)/60,($1%($1%3600)/60)}'
