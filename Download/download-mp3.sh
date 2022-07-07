e341eb68159ad7a55d08e8ac061a92a2ba4c998c
Mohamed Hafez <mohamedhafezmansour@gmail.com>
2022-07-07 03:04:50 +0200

    ✨ Downlaod single file with thumbnail

---
+++ Download/download-mp3.sh
@@ -0,0 +1,12 @@
+#! /usr/bin/bash
+# Move to Music folder
+cd ~/
+mkdir Music || true
+cd ~/Music
+
+# Downlaod single file with thumbnail
+youtube-dl  -o '%(title)s.%(ext)s' -x --embed-thumbnail --audio-format mp3 $1
+
+# Success message
+printf "\e[1;34m Download is Done \e[0m\n\n"
+printf "\e[1;34m file in  ~/Music/ \e[0m\n\n"
