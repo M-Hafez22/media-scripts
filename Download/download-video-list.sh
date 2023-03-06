e32417492073bdcde571b7061306cf31714fd574
Mohamed Hafez <mohamedhafezmansour@gmail.com>
2023-03-06 17:32:25 +0200

    Download best video with best audio

--- Download/download-video-list.sh
+++ Download/download-video-list.sh
@@ -6,7 +6,7 @@ mkdir Videos || true
 cd ~/Videos
 
 # Downlaod Video playlist
-yt-dlp   -o "%(playlist_index)s- %(title)s.%(ext)s" -f 22 $1 
+yt-dlp   -o "%(playlist_index)s- %(title)s.%(ext)s" -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/bestvideo+bestaudio' $1 
 
 # Success message
 printf "\e[1;34m Download is Done \e[0m\n\n"
--- Download/download-video.sh
+++ Download/download-video.sh
@@ -2,6 +2,6 @@
 cd ~/
 mkdir Videos || true
 cd ~/Videos
-yt-dlp   -o '%(title)s.%(ext)s' -f 22 $1
+yt-dlp   -o '%(title)s.%(ext)s' -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/bestvideo+bestaudio' $1
 printf "\e[1;34m Download is Done \e[0m\n\n"
 printf "\e[1;34m file in  ~/Videos/ \e[0m\n\n"
